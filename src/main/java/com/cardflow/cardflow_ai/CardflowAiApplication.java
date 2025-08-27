041623e23c93d7679879115efb95c8c348538854
JEJ <228353885+jeunju528-sudo@users.noreply.github.com>
2025-08-27 21:25:36 +0900

    chore : initial Spring Boot Project setup

---
+++ src/main/java/com/cardflow/cardflow_ai/CardflowAiApplication.java
@@ -0,0 +1,13 @@
+package com.cardflow.cardflow_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardflowAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardflowAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cardflow/cardflow_ai/CardflowAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cardflow.cardflow_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardflowAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
